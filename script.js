011f73452c8c99e99ef33e55e4eacd4018c97248
panda-404-2025 <angeloischeng1995@gmail.com>
2025-09-28 15:16:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Zhuo'an✌🏽","Angelois","a computer science student"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop: true
+})
